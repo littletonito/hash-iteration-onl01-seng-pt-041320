6cd4888f0dc9ddf5cf70fec6ca957f920e50ea5a
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-26 18:09:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,13 +5,13 @@
 # }
 
 
-
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
+## bonus
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
